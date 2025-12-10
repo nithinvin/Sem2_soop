f65508dbd9559ec9a7d161bb6a877befc01cd792
Vinayagamoorthy Kuppusamy <kvinayagamoorthy@gmail.com>
2025-12-10 18:40:13 +0530

    Program to count vowels in the input string

---
+++ Problems_20251210/vowels.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>   // for tolower()
+#include <string.h>  // for fgets()
+
+int main() {
+    char str[1024];
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  // Read input including spaces
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            count++;
+        }
+    }
+
+    printf("Number of vowels = %d\n", count);
+
+    return 0;
+}
