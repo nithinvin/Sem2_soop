f83f5847b455d739e16ea212b263ec5ea25201c9
Your Name <kvinayagamoorthy@gmail.com>
2026-01-14 20:10:58 +0530

    Row wise and Column wise sorting on orignal matrix

--- Sem2_DSA/DSA_Lab2_20260114/Q3_Matrix_Sorting_Row_And_Columnwise.c
+++ Sem2_DSA/DSA_Lab2_20260114/Q3_Matrix_Sorting_Row_And_Columnwise.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+/* Insertion sort for 1D array */
 void insertionSort(int arr[], int n)
 {
     int i, key, j;
@@ -18,31 +19,34 @@ void insertionSort(int arr[], int n)
     }
 }
 
+/* Sort each row in non-decreasing order */
 void sortRows(int mat[10][10], int r, int c)
 {
     for (int i = 0; i < r; i++)
         insertionSort(mat[i], c);
 }
 
+/* Sort each column in non-decreasing order */
 void sortColumns(int mat[10][10], int r, int c)
 {
     int temp[10];
 
     for (int j = 0; j < c; j++)
     {
-        /* copy column into temp array */
+        /* Copy column into temp array */
         for (int i = 0; i < r; i++)
             temp[i] = mat[i][j];
 
-        /* sort the column */
+        /* Sort the column */
         insertionSort(temp, r);
 
-        /* copy back to matrix */
+        /* Copy back into matrix */
         for (int i = 0; i < r; i++)
             mat[i][j] = temp[i];
     }
 }
 
+/* Print matrix */
 void printMatrix(int mat[10][10], int r, int c)
 {
     for (int i = 0; i < r; i++)
@@ -55,23 +59,32 @@ void printMatrix(int mat[10][10], int r, int c)
 
 int main()
 {
-    int mat[10][10], r, c;
+    int mat[10][10], rowMat[10][10], colMat[10][10];
+    int r, c;
 
-    printf("Enter rows and columns:\n");
+    printf("Enter number of rows and columns:\n");
     scanf("%d %d", &r, &c);
 
     printf("Enter matrix elements:\n");
     for (int i = 0; i < r; i++)
+    {
         for (int j = 0; j < c; j++)
+        {
             scanf("%d", &mat[i][j]);
+            rowMat[i][j] = mat[i][j];   /* copy for row-wise sorting */
+            colMat[i][j] = mat[i][j];   /* copy for column-wise sorting */
+        }
+    }
 
-    sortRows(mat, r, c);
+    /* Row-wise sorting */
+    sortRows(rowMat, r, c);
     printf("\nRow-wise sorted matrix:\n");
-    printMatrix(mat, r, c);
+    printMatrix(rowMat, r, c);
 
-    sortColumns(mat, r, c);
+    /* Column-wise sorting */
+    sortColumns(colMat, r, c);
     printf("\nColumn-wise sorted matrix:\n");
-    printMatrix(mat, r, c);
+    printMatrix(colMat, r, c);
 
     return 0;
 }
